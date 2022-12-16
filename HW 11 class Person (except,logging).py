d4d2fe240918c2c4fe2a285e5f9a7df7b2f294bf
Herman_G <sergiygisar@gmail.com>
2022-12-16 10:39:31 +0200

    Add file

--- HW 11 class Person (except,logging).py
+++ HW 11 class Person (except,logging).py
@@ -13,7 +13,11 @@ logging.basicConfig( filename="logs/my_log.log",level=logging.INFO, format='%(as
 
 # Подію додавання нового студента до групи необхідно фіксувати у лог-файлі.
 
-
+class Too_many_students (Exception):
+    def __init__(self, message):
+        self.message=message
+    def __str__(self):
+        return f"{self.message}"
 
 class Person:
 
@@ -44,10 +48,18 @@ class Group:
         self.__students = []
         self.max_student = max_students
 
+
     def add(self, student):
+
         if student not in self.__students and len(self.__students) < self.max_student:
             self.__students.append(student)
             logging.info(student)
+        else:
+            raise Too_many_students("Too_many_students")
+
+
+
+
 
     def remove(self, student):
         if student in self.__students:
@@ -67,15 +79,26 @@ class Group:
         return f'{self.title}\n\n' + '\n'.join(map(str, self.__students))
 
 
-a = Group('Python', max_students=5)
+a = Group('Python', max_students=10)
 
+try:
+    a.add(Student('Roman', 'Romanov', 21, "Romanovych"))
+    a.add(Student('Oleg', 'Lyah', 21, "Dmytrovych"))
+    a.add(Student('Maksym', 'Kovtunov', 30, "Dmytrovych"))
+    a.add(Student('Oleksandr', 'Melnuk', 21, "Kostyantunovych"))
+    a.add(Student('Taras', 'Topolya', 22, "Petrovych"))
+    a.add(Student('Oleg', 'Skrypka', 23, "Anatoliyovych"))
+    a.add(Student('Seghiy', 'Gorbatenko', 20, "Romanovych"))
+    a.add(Student('Bogdan', 'Byt', 24, "Bogdanovych"))
+    a.add(Student('Oleksandr', 'Lyah', 25, "Dmytrovych"))
+    a.add(Student('Maksym', 'Lyah', 26, "Anatoliyovych"))
+    a.add(Student('Taras', 'Lyah', 21, "Kostyantunovych"))
 
-a.add(Student('Roman', 'Romanov', 21, "Romanovych"))
-a.add(Student('Oleg', 'Lyah', 21, "Dmytrovych"))
+except Too_many_students:
+    print("Too_many_students")
 
-print(a)
 print()
+print(a)
 res = a.search(21)
 for item in res:
     print(item.name, item.middle_name,  item.age)
-
--- Logging.py
+++ Logging.py
@@ -1,33 +1,100 @@
 
 # Подію додавання нового студента до групи необхідно фіксувати у лог-файлі.
 
-import logging
-logger=logging.getLogger("logging.py")
+# import logging
+# logger=logging.getLogger("logging.py")
+#
+# logger.setLevel(logging.INFO)
+#
+#
+# formatter=logging.Formatter('%(asctime)s -%(name)s -%(levelname)s -%(message)s')
+#
+# cons=logging.StreamHandler()
+# cons.setLevel(logging.WARNING)
+# cons.setFormatter(formatter)
+#
+#
+#
+# file=logging.FileHandler("logs/my_log.log")
+# file.setLevel(logging.INFO)
+# file.setFormatter(formatter)
+#
+# logger.addHandler(file)
+# logger.addHandler(cons)
+#
+#
+#
+#
+# if __name__ =='logging.py':
+#     logger.info("Add student")
+#     logger.warning("Something was wrong")
 
-logger.setLevel(logging.INFO)
 
 
-formatter=logging.Formatter('%(asctime)s -%(name)s -%(levelname)s -%(message)s')
-
-cons=logging.StreamHandler()
-cons.setLevel(logging.WARNING)
-cons.setFormatter(formatter)
-
-
-
-file=logging.FileHandler("logs/my_log.log")
-file.setLevel(logging.INFO)
-file.setFormatter(formatter)
-
-logger.addHandler(file)
-logger.addHandler(cons)
+class Incorrect_price (Exception):
+    def __init__(self, message):
+        self.message=message
+    def __str__(self):
+        return f"{self.message}"
 
 
+class Goods:
+    def __init__(self, title, price, group_of_goods):
+        if price <= 0:
+            raise Incorrect_price("Sometimes was wrong")
 
 
-if __name__ =='logging.py':
-    logger.info("Add student")
-    logger.warning("Something was wrong")
+        self.title = title
+        self.price = price
+        self.group_of_goods=group_of_goods
+
+
+    def __str__(self):
+        return f'{self.title}: {self.price} UAH, Group of goods: {self.group_of_goods}'
 
 
 
+class Сustomer:
+    def __init__(self, name,  surname, phone_number):
+        self.name = name
+        self.surname = surname
+        self.phone_number=phone_number
+
+    def __str__(self):
+        return f'{self.name} {self.surname}, phone_number: {self.phone_number}'
+
+
+
+class Order:
+    def __init__(self, name):
+        self.name=name
+        self.price_of_goods=[]
+        self.list_of_goods = []
+
+    def add (self, good:Goods):
+        if good not in self.list_of_goods:
+            self.list_of_goods.append(good)
+            self.price_of_goods.append(good.price)
+
+    def __str__(self):
+        return f'{self.name} ({customerB})' +'\n' +'\n' +'\n'.join(map(str, self.list_of_goods)) + '\n' + f"{'_' * 44}" '\n'+ 'Total cost:  ' + f'{sum(map(int, self.price_of_goods))} UAH'
+
+try:
+    cherry=Goods("Cherry",100, "fruit")
+    potatoes=Goods("Potatoes",0,'vegetables')
+    limon=Goods("Limon",80,"fruit")
+
+
+    customerA=Сustomer("Alexander","Panchuk", "+380930252500")
+    customerB=Сustomer("Maksym","Evych","+380675544333")
+
+    order_1 =Order("Consumer Order 09_12_2022")
+    order_1.add(cherry)
+    order_1.add(limon)
+    order_1.add(potatoes)
+    print(order_1)
+
+
+
+except Incorrect_price:
+    print("The price of the product is incorrect")

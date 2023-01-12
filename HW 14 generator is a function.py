9c0b2742df9d1eed839118b3d227df15cfa1ed90
Herman_G <sergiygisar@gmail.com>
2023-01-13 01:49:18 +0200

    Add file

--- HW 14 generator is a function.py
+++ HW 14 generator is a function.py
@@ -1,7 +1,6 @@
 
  # 1. Реалізуйте генераторну функцію, яка повертатиме по одному члену геометричної прогресії із зазначеним множником.
  # Генератор повинен зупинити свою роботу або після досягнення зазначеного елементу, або при передачі команди на завершення.
-
 def geom_progression(number,denominator):
     print(number)
     res=denominator*number
@@ -9,7 +8,7 @@ def geom_progression(number,denominator):
         yield res
         number=res
         res=denominator*number
-    return res
+
 seq = geom_progression(5, 10)
 
 for item in seq:
@@ -19,10 +18,35 @@ for item in seq:
 
 
 # 2. Реалізуйте свій аналог генераторної функції range().
+def unique_range(first_num, last_num = (), step = ()):
+    if not last_num:
+        last_num,first_num = first_num,0
+    if not step:
+        step = 1
+    follow_ = first_num
+    while follow_ < last_num:
+        yield follow_
+        first_num = follow_
+        follow_ = first_num + step
+seq = unique_range(1,5)
+
+#
+#
+# print (next(seq))
+# print (next(seq))
+# print (next(seq))
+# print (next(seq))
+# print (next(seq))
+# print (next(seq))
+# print (next(seq))
+# print (next(seq))
+for item in seq:
+    print(list(seq))
+
 # 3. Напишіть функцію-генератор, яка повертатиме прості числа.
 #  Верхня межа діапазону повинна бути задана параметром цієї функції.
 # 4. Напишіть генераторний вираз для заповнення списку.
 #  Список повинен бути заповнений кубами чисел від 2 до вказаної вами величини.
 import random
-gen = (item**3 for item in range(random.randint(2,50)))
+gen = (item**3 for item in range(random.randint(2,20)))
 print(list(gen))
